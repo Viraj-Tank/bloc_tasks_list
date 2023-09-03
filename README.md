594eea3cf0df98a13ea3fab4d7a7cd408aa6c6cd
VirajTank <virajtank98@gmail.com>
2023-09-03 10:48:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# bloc_tasks_list
+
+Tasks application with Bloc state management build using Flutter framework.
+
---
+++ android/app/src/main/kotlin/com/novuspax/bloc_tasks_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.novuspax.bloc_tasks_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
